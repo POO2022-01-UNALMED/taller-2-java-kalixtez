2a8b01c26a2764268cb92bb21fd9273897487839
kalixtez <64610615+kalixtez@users.noreply.github.com>
2022-03-19 15:36:31 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -14,7 +14,13 @@ public class Auto
 	
 	public int cantidadAsientos()
 	{
-		return this.asientos.length;
+		int cantidad = 0;
+		for(int i = 0; i < this.asientos.length; i++)
+		{
+			if(this.asientos[i] != null)
+				cantidad++;
+		}
+		return cantidad;
 	}
 	
 	public String verificarIntegridad()
@@ -22,7 +28,7 @@ public class Auto
 		if(this.registro != this.motor.registro)
 			return "Las piezas no son originales";
 		
-		for(int i = 0; i <= this.asientos.length; i++)
+		for(int i = 0; i < this.asientos.length; i++)
 		{
 			if (this.asientos[i] != null)
 			{
